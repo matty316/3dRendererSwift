7f309d385bf058eb26269b7f550ce74b11498123
Matthew Reed <mattreed1890@gmail.com>
2025-02-07 07:36:47 -0600

    Initial Commit

---
+++ Engine/EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngineApp.swift
+//  Engine
+//
+//  Created by matty on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EngineTests/EngineTests.swift
@@ -0,0 +1,17 @@
+//
+//  EngineTests.swift
+//  EngineTests
+//
+//  Created by matty on 2/7/25.
+//
+
+import Testing
+@testable import Engine
+
+struct EngineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
